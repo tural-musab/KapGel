c545c3a293bdf29939be8d439fbef61416fef692
Tural Mammadov <tural.musab.osmanli@gmail.com>
2025-09-30 12:08:07 +0400

    fix(drizzle): update config to use driver property

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 
-export default {
+export default defineConfig({
   schema: './db/schema.ts',
   out: './db/migrations',
-  dialect: 'postgresql',
+  driver: 'pg',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   },
-} satisfies Config;
+});
